febef3d29344ce342e9b976bfd6ee73c60911cc7
Katarzyna Płoska <pkatarzyna@grefekt.pl>
2024-09-01 21:30:45 +0200

    chapter4 - tailwind,icoify + components

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
-import { supabase } from '@/lib/supabaseClient'
-
-console.log(supabase)
+import TopNavbar from '@/components/Layout/TopNavbar.vue'
+import Sidebar from '@/components/Layout/Sidebar.vue'
 </script>
 
 <template>
-  <main>
-    <RouterView />
-  </main>
+  <Sidebar />
+  <div class="flex flex-col lg:ml-52 ml-16 transition-[margin]">
+    <TopNavbar />
+    <main>
+      <RouterView />
+    </main>
+  </div>
 </template>
---
+++ src/components/Layout/Sidebar.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import SidebarLinks from './SidebarLinks.vue'
+
+const links = [
+  {
+    title: 'Dashboard',
+    to: '/',
+    icon: 'lucide:house'
+  },
+  {
+    title: 'Projects',
+    to: '/projects',
+    icon: 'lucide:building-2'
+  },
+  {
+    title: 'My Tasks',
+    to: '/tasks',
+    icon: 'lucide:badge-check'
+  }
+]
+
+const accountLinks = [
+  {
+    title: 'Profile',
+    to: '/profile',
+    icon: 'lucide:user'
+  },
+  {
+    title: 'Settings',
+    to: '/settings',
+    icon: 'lucide:settings'
+  },
+  {
+    title: 'Sign Out',
+    to: '/signout',
+    icon: 'lucide:log-out'
+  }
+]
+</script>
+
+<template>
+  <aside
+    class="flex flex-col h-screen gap-2 border-r fixed bg-muted/40 lg:w-52 w-16 transition-[width]"
+  >
+    <div class="flex h-16 items-center border-b px-2 lg:px-4 shrink-0 gap-1 justify-between">
+      <button variant="outline" size="icon" class="w-8 h-8">
+        <iconify-icon icon="lucide:menu"></iconify-icon>
+      </button>
+
+      <button variant="outline" size="icon" class="w-8 h-8">
+        <iconify-icon icon="lucide:plus"></iconify-icon>
+      </button>
+    </div>
+
+    <nav class="flex flex-col gap-2 justify-between h-full relative">
+      <div>
+        <SidebarLinks :links="links" />
+      </div>
+
+      <div class="border-y text-center bg-background py-3">
+        <SidebarLinks :links="accountLinks" />
+      </div>
+    </nav>
+  </aside>
+</template>
