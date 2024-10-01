747b1bf27bd043d64e25dff487745081d0988d92
Katarzyna Płoska <pkatarzyna@grefekt.pl>
2024-10-01 13:34:29 +0200

    RouterLink

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<!-- <script setup lang="ts">
 import { supabase } from '@/lib/supabaseClient'
 import { ref } from 'vue'
 import type { Tables } from '../../../database/types'
@@ -22,4 +22,60 @@ const projects = ref<Tables<'projects'>[] | null>(null)
       </li>
     </ul>
   </div>
+</template> -->
+
+<script setup lang="ts">
+import { supabase } from '@/lib/supabaseClient'
+import { h, ref } from 'vue'
+import type { Tables } from '../../../database/types'
+import type { ColumnDef } from '@tanstack/vue-table'
+import DataTable from '@/components/ui/data-table/DataTable.vue'
+import { RouterLink } from 'vue-router'
+
+const projects = ref<Tables<'projects'>[] | null>(null)
+;(async () => {
+  const { data, error } = await supabase.from('projects').select()
+  if (error) console.log(error)
+  projects.value = data
+  console.log('projects: ', projects.value)
+})()
+
+const columns: ColumnDef<Tables<'projects'>>[] = [
+  {
+    accessorKey: 'name',
+    header: () => h('div', { class: 'text-left' }, 'Name'),
+    cell: ({ row }) => {
+      return h(
+        RouterLink,
+        {
+          to: `/projects/${row.original.slug}`,
+          class: 'text-left font-medium hover:bg-muted block w-full'
+        },
+        () => row.getValue('name')
+      )
+    }
+  },
+  {
+    accessorKey: 'status',
+    header: () => h('div', { class: 'text-left' }, 'Status'),
+    cell: ({ row }) => {
+      return h('div', { class: 'text-left font-medium' }, row.getValue('status'))
+    }
+  },
+  {
+    accessorKey: 'collaborators',
+    header: () => h('div', { class: 'text-left' }, 'Collaborators'),
+    cell: ({ row }) => {
+      return h(
+        'div',
+        { class: 'text-left font-medium' },
+        JSON.stringify(row.getValue('collaborators'))
+      )
+    }
+  }
+]
+</script>
+
+<template>
+  <DataTable v-if="projects" :columns="columns" :data="projects" />
 </template>
--- src/pages/tasks/index.vue
+++ src/pages/tasks/index.vue
@@ -4,22 +4,30 @@ import { h, ref } from 'vue'
 import type { Tables } from '../../../database/types'
 import type { ColumnDef } from '@tanstack/vue-table'
 import DataTable from '@/components/ui/data-table/DataTable.vue'
+import { RouterLink } from 'vue-router'
 
 const tasks = ref<Tables<'tasks'>[] | null>(null)
 ;(async () => {
   const { data, error } = await supabase.from('tasks').select()
+
   if (error) console.log(error)
+
   tasks.value = data
 })()
-
-// import { h } from 'vue'
 
 const columns: ColumnDef<Tables<'tasks'>>[] = [
   {
     accessorKey: 'name',
     header: () => h('div', { class: 'text-left' }, 'Name'),
     cell: ({ row }) => {
-      return h('div', { class: 'text-left font-medium' }, row.getValue('name'))
+      return h(
+        RouterLink,
+        {
+          to: `/tasks/${row.original.id}`,
+          class: 'text-left font-medium hover:bg-muted block w-full'
+        },
+        () => row.getValue('name')
+      )
     }
   },
   {
